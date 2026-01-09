efd433b54df65ececc3b3aaf163661974d8d471f
Rayane Rocha <141773781+Rayrraio@users.noreply.github.com>
2026-01-09 03:21:02 +0000

    feat: conclui automação E2E com testes positivos/negativos e relatório html

--- test_venda.py
+++ test_venda.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright
 import os
 
+# TESTE 1: Sucesso (Login e Compra)
 def test_fluxo_venda_completo():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
@@ -8,21 +9,31 @@ def test_fluxo_venda_completo():
         caminho = "file://" + os.path.abspath("index.html")
         page.goto(caminho)
 
-        # 1. Login
         page.fill("#user", "admin")
         page.fill("#pass", "123")
         page.click("#btn-login")
-
-        # 2. Adicionar ao Carrinho
         page.click("#add-item")
-
-        # 3. Checkout (Preencher dados e finalizar)
         page.fill("#cartao", "4444 5555 6666 7777")
         page.fill("#nome", "RAYANE ROCHA")
-        
-        # Lida com o alerta de sucesso
         page.on("dialog", lambda d: d.accept())
         page.click("#btn-finalizar")
         page.screenshot(path="resultado_venda.png")
-        print("✅ Teste de ponta a ponta concluído com sucesso!")
+        browser.close()
+
+# TESTE 2: Caminho Infeliz (Senha Errada)
+def test_login_senha_incorreta():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        caminho = "file://" + os.path.abspath("index.html")
+        page.goto(caminho)
+
+        page.fill("#user", "admin")
+        page.fill("#pass", "SENHA_ERRADA")
+        page.click("#btn-login")
+
+        # Verifica se a loja continuou escondida
+        is_hidden = page.locator("#vitrine").is_hidden()
+        assert is_hidden
+        print("✅ Sucesso: O sistema bloqueou o acesso com senha errada!")
         browser.close()
