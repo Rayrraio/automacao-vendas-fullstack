845a04c7ad3d1d1e0d26f45e1f5ec88408b6913f
Rayane Rocha <141773781+Rayrraio@users.noreply.github.com>
2026-01-09 02:23:53 +0000

    Adicionando marketplace e fluxo de checkout completo

--- test_venda.py
+++ test_venda.py
@@ -1,14 +1,28 @@
 from playwright.sync_api import sync_playwright
 import os
 
-def test_login_sucesso():
+def test_fluxo_venda_completo():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         caminho = "file://" + os.path.abspath("index.html")
         page.goto(caminho)
+
+        # 1. Login
         page.fill("#user", "admin")
         page.fill("#pass", "123")
         page.click("#btn-login")
-        print("✅ Fluxo de login testado com sucesso!")
+
+        # 2. Adicionar ao Carrinho
+        page.click("#add-item")
+
+        # 3. Checkout (Preencher dados e finalizar)
+        page.fill("#cartao", "4444 5555 6666 7777")
+        page.fill("#nome", "RAYANE ROCHA")
+        
+        # Lida com o alerta de sucesso
+        page.on("dialog", lambda d: d.accept())
+        page.click("#btn-finalizar")
+
+        print("✅ Teste de ponta a ponta concluído com sucesso!")
         browser.close()
